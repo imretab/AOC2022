f4039b78a71e9f776a8b4fca2877a0febe20a0e2
imretab <45401976+imretab@users.noreply.github.com>
2022-12-02 18:59:12 +0100

    Add files via upload

---
+++ Day02/RPS.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day_02
+{
+    class RPS
+    {
+        public char YourChoice { get; set; }
+        public char ElfChoice { get; set; }
+        public RPS(char ec, char yc)
+        {
+            YourChoice = yc;
+            ElfChoice = ec;
+        }
+    }
+}
