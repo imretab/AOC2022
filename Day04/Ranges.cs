ed0f350a62ac5b49280088b17b79637e2d779abe
imretab <45401976+imretab@users.noreply.github.com>
2022-12-04 17:55:21 +0100

    Add files via upload

---
+++ Day04/Ranges.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day_04
+{
+    class Ranges
+    {
+        public int Start { get; set; }
+        public int End { get; set; }
+        public Ranges(int r1, int r2)
+        {
+            Start = r1;
+            End = r2;
+        }
+    }
+}
